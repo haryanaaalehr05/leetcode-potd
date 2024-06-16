8de895a7b16f697743790b7ef44f7e2af980ef56
jugadu_enginer <138850887+haryanaaalehr05@users.noreply.github.com>
2024-06-16 10:32:20 +0530

    Time: 20 ms (40.36%), Space: 10.1 MB (62.00%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int count = 0;
+
+        // Single character palindromes
+        for (int i = 0; i < n; ++i) {
+            dp[i][i] = true;
+            count++;
+        }
+
+        // Two character palindromes
+        for (int i = 0; i < n - 1; ++i) {
+            if (s[i] == s[i + 1]) {
+                dp[i][i + 1] = true;
+                count++;
+            }
+        }
+
+        // Substrings longer than two characters
+        for (int length = 3; length <= n; ++length) {
+            for (int i = 0; i <= n - length; ++i) {
+                int j = i + length - 1;
+                if (s[i] == s[j] && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
